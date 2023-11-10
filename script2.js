837aa97d9faec706c0c0954f9057463cf0e86eda
Luca <luca.grabherr@student.htldornbirn.at>
2023-11-10 14:18:18 +0100

    Spiele-Seite

--- script2.js
+++ script2.js
@@ -1,5 +1,21 @@
+function insertRow(data) {
+    let output = "";
+  
+    data.forEach((team, index) => {
+        let row = `
+            <div class="box1date">
+            <div>${index + 1}</div>
+            <div class="date1">${team.matchDateTime}</div>
+            </div>`;
+  
+        output += row;
+    });
+  
+    document.getElementById("containerID2").innerHTML = output;
+  }
+
 function loadData() {
-    fetch("https://api.openligadb.de/getbltable/öbl1/2023")
+    fetch("https://api.openligadb.de/getbltable/öbl1/2023/")
         .then((result) =>
             result.json()
         )
