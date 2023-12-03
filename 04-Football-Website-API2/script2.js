591641d3d1cdba54cba586cd61485313f46b9d83
lugschi <75794561+lugschi@users.noreply.github.com>
2023-12-03 17:28:52 +0100

    Website-Finished

--- 04-Football-Website-API2/script2.js
+++ 04-Football-Website-API2/script2.js
@@ -4,14 +4,16 @@ function insertRow(data) {
     data.forEach((team, index) => {
         if (team.matchResults && team.matchResults.length >= 2) {
             let row = `
+            <div class="fatherbox">
             <div class="motherbox">
                 <div class="box1date">
                     <div class="games">
-                    <div class="teamName1">${team.team1.teamName}</div>
-                    <div class="matchResult">${team.matchResults[1].pointsTeam1} : ${team.matchResults[1].pointsTeam2} </div>                    
-                    <div class="teamName2">${team.team2.teamName}</div>
+                    <div class="teamName1"><div class="NameStyle">${team.team1.teamName}</div><img src="${team.team1.teamIconUrl}" width="30" height="30"></div>
+                    <div class="matchResult">${team.matchResults[1].pointsTeam1} - ${team.matchResults[1].pointsTeam2} </div>                    
+                    <div class="teamName2"><img src="${team.team2.teamIconUrl}" width="30" height="30"><div class="NameStyle">${team.team2.teamName}</div></div>
                     </div>
                 </div>
+            </div>
             </div>`;
 
             output += row;
