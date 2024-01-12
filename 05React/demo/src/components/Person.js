d0b5d82a021311331e466931118b41cbbf18a944
Luca <luca.grabherr@student.htldornbirn.at>
2024-01-12 14:45:19 +0100

    Card-Begonnen

--- 05React/demo/src/components/Person.js
+++
@@ -1,9 +0,0 @@
-export default function Person({name, description}){
-    console.log("name:" , name);
-    return(
-        <div class="person">
-            <h1>{name}</h1>
-            <p>{description}</p>
-        </div>
-    )
-}
