66f9bbc6b3ee7a35055a14b2e5a7085bb8d4ed6c
Luca <luca.grabherr@student.htldornbirn.at>
2024-01-17 12:12:13 +0100

    Card und PeopleContainer Component gemacht

---
+++ 05-React/02-cards-data/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Card(name, title, imageUri) {
+  return (
+    <div>
+        <div>
+            <h2>Hans</h2>
+            <h4>CEO</h4>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
