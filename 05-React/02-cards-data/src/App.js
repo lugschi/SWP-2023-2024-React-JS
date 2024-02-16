907c910a055424ad080f1d429df78e20f3544f63
Luca <luca.grabherr@student.htldornbirn.at>
2024-02-16 14:45:30 +0100

    API eingebunden

--- 05-React/02-cards-data/src/App.js
+++ 05-React/02-cards-data/src/App.js
@@ -1,18 +1,12 @@
+import { useEffect } from 'react';
 import './App.css';
-import Card from './components/Card';
+import PeopleContainer from './components/PeopleContainer';
 
 function App() {
+
   return (
-    <div className="father">
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/200/300"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/201/300"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/202/300"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/203/300"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/204/300"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/205/300"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/200/301"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/200/302"/>
-      <Card firstname="Hans" lastname="Zimmer" title="Investor" imageUri="https://picsum.photos/200/303"/>
+    <div>
+      <PeopleContainer/>
     </div>
   );
 }
--- 05-React/02-cards-data/src/components/PeopleContainer.js
+++ 05-React/02-cards-data/src/components/PeopleContainer.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from './Card'
 
 export default function PeopleContainer() {
+
+  const [people, setPeople] = useState([]);
+
+useEffect(()=>{
+  fetch("https://65cf5380bdb50d5e5f5b0746.mockapi.io/person").then(
+    (res) => res.json().then((data) => {
+      setPeople(data);
+    }));
+},[])
+
   return (
+    
     <div>
-        <h1>People</h1>
-        <div>
-            <div>person1</div>
-            <div>person1</div>
-            <div>person1</div>
-        </div>
+      <div className="father">
+      {people.map(person => {
+        return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle}/>
+      })}
     </div>
+    </div>
+
+    
   )
 }
