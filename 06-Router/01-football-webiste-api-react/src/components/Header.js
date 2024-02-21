bcfaa92a722c66d24e94babc72199198af26d5fd
Luca <luca.grabherr@student.htldornbirn.at>
2024-02-21 12:18:35 +0100

    Header-Football-Api

---
+++ 06-Router/01-football-webiste-api-react/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <div class="logo">
+                <img src="./logo.png" alt="" srcset="" width="42" height="42"></img>
+            </div>
+            <div class="league">
+                <div class="bundesliga">
+                    Bundesliga
+                </div>
+                <div class="country">
+                    Österreich
+                </div>
+            </div>
+            <div class="date">
+                <div>
+                    2023/24
+                </div>
+            </div>
+
+            <div class="detailSelection">
+            <div class="selection">Übersicht</div>
+            <div class="selection" id="showSelection">Tabelle</div>
+            <div class="selection">Spiele</div>
+            <div class="selection">Statistiken</div>
+            <div class="selection">Transfers</div>
+        </div>
+
+        <div class="borderline">
+
+        </div>
+    </div>
+  )
+}
