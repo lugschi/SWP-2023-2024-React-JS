680d1fd88716a97b1f9694f79ce718d128af012c
Luca <luca.grabherr@student.htldornbirn.at>
2024-02-21 11:23:16 +0100

    Demo Filter fertig

--- 05-React/02-cards-data/src/components/PeopleContainer.js
+++ 05-React/02-cards-data/src/components/PeopleContainer.js
@@ -4,19 +4,33 @@ import Card from './Card'
 export default function PeopleContainer() {
 
   const [people, setPeople] = useState([]);
+  const [filteredPeople, setFilteredPeople] = useState([]);
 
 useEffect(()=>{
   fetch("https://65cf5380bdb50d5e5f5b0746.mockapi.io/person").then(
     (res) => res.json().then((data) => {
       setPeople(data);
+      setFilteredPeople(data)
     }));
 },[])
+
+const filterPeople = (filter) =>{
+  let filtered = people.filter(person => person.name.includes(filter));
+  setFilteredPeople(filtered);
+}
 
   return (
     
     <div>
+
+      <div className='searchContainer'>
+        <input className='searchBar' type='text' placeholder='Search' onChange={(el)=>{
+              filterPeople(el.target.value);
+        }}></input>
+      </div>
+
       <div className="father">
-      {people.map(person => {
+      {filteredPeople.map(person => {
         return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle}/>
       })}
     </div>
