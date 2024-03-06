dcc87f758c952cd2d984cd361633e2cc48c70364
lugschi <75794561+lugschi@users.noreply.github.com>
2024-03-06 18:19:37 +0100

    Router eingebungen

--- 06-Router/01-football-webiste-api-react/src/App.js
+++ 06-Router/01-football-webiste-api-react/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import TablePage from "./page/TablePage"
+import TablePage from "./page/TablePage";
+import MatchPage from './page/MatchPage';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <TablePage/>
+    },
+    {
+      path: "/Match",
+      element: <MatchPage/>
+    }
+  ]);
+
   return (
     <div className="container">
-      <TablePage/>
+      <RouterProvider router={router} />      
     </div>
   );
 }
